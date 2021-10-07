9acdf47066d81b34f6ea0bd788f6366e1fff7539
imjeongjun <jjlim1004@naver.com>
2021-10-08 01:07:15 +0900

    Role 변경과 Member 생성자 변경으로 인한 member login() 변경

--- src/main/java/org/mymafia/mafiaGame/service/MemberServiceImpl.java
+++ src/main/java/org/mymafia/mafiaGame/service/MemberServiceImpl.java
@@ -1,6 +1,7 @@
 package org.mymafia.mafiaGame.service;
 
 import org.mymafia.mafiaGame.vo.dto.MemberDTO;
+import org.mymafia.mafiaGame.vo.dto.Role;
 import org.mymafia.mafiaGame.vo.entity.Member;
 import org.mymafia.mafiaGame.vo.entity.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +14,13 @@ public class MemberServiceImpl implements MemberService{
 
     @Override
     public int Join(MemberDTO memberDto) {
+        memberDto.setMemberRole(Role.USER);
         Member member = Member.builder()
                 .memberId(memberDto.getMemberId())
                 .memberPw(memberDto.getMemberPw())
                 .memberRole(memberDto.getMemberRole())
+                .memberEmail(memberDto.getEmail())
+                .memberName(memberDto.getMemberName())
                 .build();
         memberRepository.save(member);
         return 0;
