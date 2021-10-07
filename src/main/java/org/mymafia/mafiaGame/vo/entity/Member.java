8f410e70e0da6818050a679ea6f697ace6f7eab9
imjeongjun <jjlim1004@naver.com>
2021-10-07 17:06:41 +0900

    회원 entity

--- src/main/java/org/mymafia/mafiaGame/vo/entity/Member.java
+++ src/main/java/org/mymafia/mafiaGame/vo/entity/Member.java
@@ -1,11 +1,9 @@
 package org.mymafia.mafiaGame.vo.entity;
 
 import lombok.*;
+import org.mymafia.mafiaGame.vo.dto.Role;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity(name = "member")
 @NoArgsConstructor
@@ -15,18 +13,33 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long memberNumber;
 
-
+    @Column(nullable=false)
     private String memberId;
 
+    @Column(nullable=false)
     private String memberPw;
 
+    @Column(nullable=false)
     private String name;
 
+    @Column
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+
+    public String getRoleKey(){
+        return this.role.getKey();
+    }
+
     @Builder
-    public Member(Long memberNumber, String memberId, String memberPw, String name) {
+    public Member(Long memberNumber, String memberId, String memberPw, String name, Role role) {
         this.memberNumber = memberNumber;
         this.memberId = memberId;
         this.memberPw = memberPw;
         this.name = name;
+        this.role = role;
     }
 }
