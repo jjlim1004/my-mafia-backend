097c8778209653c9134f73e6c3a732d4f6037c21
imjeongjun <jjlim1004@naver.com>
2021-10-13 22:44:59 +0900

    SecurityConfig 정상 작동 확인

--- src/main/java/org/mymafia/mafiaGame/config/SecurityConfig.java
+++ src/main/java/org/mymafia/mafiaGame/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -15,4 +16,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.httpBasic().disable();
     }
+
 }
+
+
