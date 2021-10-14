d5f0e305cd23e5b2a2e4a9707dc9882de97eb0cb
imjeongjun <jjlim1004@naver.com>
2021-10-14 11:30:23 +0900

    사용자의 email과 닉네임을 요청하는 api 까지 추가한 loginController

--- src/main/java/org/mymafia/mafiaGame/controlloer/commonController/loginController.java
+++ src/main/java/org/mymafia/mafiaGame/controlloer/commonController/loginController.java
@@ -6,6 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class loginController {
 
@@ -18,12 +22,30 @@ public class loginController {
     }
 
     @GetMapping("/login")
-    public String loginPage(@RequestParam("code") String code){
+    public String loginPage(@RequestParam("code") String code , HttpSession session){
         String access_token = kakaoAPI.getAccessToken(code);
+
         System.out.println("controller access_token : " + access_token);
+
+        HashMap<String,Object> userInfo = kakaoAPI.getUserInfo(access_token);
+
+        for(Map.Entry<String,Object> entry : userInfo.entrySet()){
+            System.out.println("!!! " + entry.getKey() + "  : " + entry.getValue());
+        }
+
+        if (userInfo.get("email") != null) {
+            session.setAttribute("userId", userInfo.get("email"));
+            session.setAttribute("access_Token", access_token);
+        }
+
+
         return "index";
     }
 
+    @GetMapping("/logout")
+    public String logout(HttpSession session){
 
+        return "index";
+    }
 }
 
