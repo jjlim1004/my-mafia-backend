e55d2b705d6f3f797a3ea6a444dd4d0a6e822a6f
imjeongjun <jjlim1004@naver.com>
2021-10-15 21:08:03 +0900

    게임 로비 페이지를 제어하기 위한 Controller

---
+++ src/main/java/org/mymafia/mafiaGame/controlloer/roomController/LobbyController.java
@@ -0,0 +1,14 @@
+package org.mymafia.mafiaGame.controlloer.roomController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LobbyController {
+
+    @GetMapping("lobbyPage")
+    public void lobbyPage(){
+
+    }
+
+}
