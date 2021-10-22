d95656dc9a5c8abc9db5ed67ca2904bfdc9b95d2
imjeongjun <jjlim1004@naver.com>
2021-10-22 21:58:20 +0900

    jwt 세션 로그인을 위해서 재설정 중인 것들

--- src/main/java/org/mymafia/mafiaGame/config/SecurityConfig.java
+++ src/main/java/org/mymafia/mafiaGame/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package org.mymafia.mafiaGame.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -11,6 +12,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @Configuration
+//웹 보안에 대한 기본적인 기능 활성화
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -23,15 +25,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         //403 에러를 방지하기 위해 csrf 해제
                     .csrf().disable()
                     //세션 관리.세션생성정책(스프링시큐리티가 생성하지도 않고 기존것을 사용하지도 않음 == jwt 방식시 사용)
-                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    //.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .authorizeRequests().antMatchers("/**").permitAll()
                 .and()
-                    .authorizeRequests().anyRequest().authenticated();
+                    .logout()
+                    .logoutSuccessUrl("/");
+
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/css/**","/script/**","image/**");
     }
+
 
 }
 
