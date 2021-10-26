d5475f626768becc5a294607e3022f42e817cc8e
imjeongjun <jjlim1004@naver.com>
2021-10-26 23:40:18 +0900

    회원 로그인 기능을 추가

--- src/main/java/org/mymafia/mafiaGame/vo/entity/MemberRepository.java
+++ src/main/java/org/mymafia/mafiaGame/vo/entity/MemberRepository.java
@@ -1,7 +1,11 @@
 package org.mymafia.mafiaGame.vo.entity;
 
+import org.mymafia.mafiaGame.vo.dto.MemberDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
 
+    @Query("select member from Member member where member.memberID =?1")
+    Member findByMemberId(MemberDTO memberDTO);
 }
